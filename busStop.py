63fbe343fa8bafa81b20b59bfe55cef28890fd0c
unknown <muhamad-usman@hotmail.com>
2020-08-09 00:50:08 +0500

    listed datasets

---
+++ busStop.py
@@ -0,0 +1,28 @@
+import overpy
+import mylib
+api = overpy.Overpass()
+import os
+# fetch all ways and nodes
+data = []
+result = api.query("""
+    (
+    node(41.5661,-88.4290,42.0992,-87.0337)["traffic_signals"];
+    );
+    (._;>;);
+    out body;
+    """)
+counter = 0
+for way in result.ways:
+    print("Name: %s" % way.tags.get("name", "n/a")),
+    print("lit: %s" % way.tags.get("lit", "n/a")),
+    print("  Highway: %s" % way.tags.get("highway", "n/a"))
+    print("  Nodes:")
+    data.append([])
+    data[counter].append("{}".format(way.tags.get("lit", "n/a")))
+    data[counter].append("{}".format(way.tags.get("name", "n/a")))
+    counter += 1
+print(data)
+#mylib.AppendCSVs(os.path.join(os.getcwd(), 'lit.csv'), mylib.ConvertToDataFrame(Data=data, Columns=['a', 'b']))
+    #for node in way.nodes:
+    #    print(node)
+    #    print("    Lat: %f, Lon: %f" % (node.lat, node.lon))
